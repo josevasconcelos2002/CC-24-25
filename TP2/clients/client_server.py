16e6fc1ea087fd0416f0bb4bfbb3c1233c32c8db
José Vasconcelos <zevasconcelos6@gmail.com>
2024-11-21 12:57:35 +0000

    Map de metricas no client_server

--- TP2/clients/client_server.py
+++ TP2/clients/client_server.py
@@ -3,10 +3,15 @@ class ClientServer:
     def __init__(self, addr, socket):
         self.address = addr
         self.socket = socket
+        self.metrics = {}
 
     def to_dict(self):
         # Retorna os dados do cliente como um dicionário
         return {
             "address": self.address,
-            "socket": self.socket
+            "socket": self.socket,
+            "metrics": self.metrics
         }
+    
+    def add_metric(self, task_id: str, metric: str):
+        self.metrics[task_id] += metric
