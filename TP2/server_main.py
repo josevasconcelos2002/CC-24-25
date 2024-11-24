b9905166c37e8cb955b4cec71f6ec3e6039bf2ed
José Vasconcelos <zevasconcelos6@gmail.com>
2024-11-24 08:44:57 +0000

    Alterações da última reunião(1)

--- TP2/server_main.py
+++ TP2/server_main.py
@@ -3,29 +3,23 @@ from clients.client import Client
 import os
 import time
 import threading
-import sys
 
 if __name__ == "__main__":
-    # sys.argv[1:] contém os argumentos passados ao script
-    if len(sys.argv) < 1:
-        print("Usage: python script_name.py <arg1>")
-        sys.exit(1)
+    # Initialize the NMS server
+    nms_server = NMS_server()
 
-    arg1 = sys.argv[1]  # Primeiro argumento
+    # Load tasks from JSON
+    current_dir = os.path.dirname(__file__)
+    json_path = os.path.join(current_dir, "tasks.json")
+    nms_server.parse_json(json_path)
 
-    print(f"Argumento 1: {arg1}")
-
-    server_ip='10.0.3.2'
+    server_ip='0.0.0.0'
     server_port=54321
-    
-    client1 = Client(server_ip, server_port, arg1 )
-    client1_thread = threading.Thread(target=client1.listen_for_datagrams)
-    client1_thread.daemon = True
-    client1_thread.start()
 
-    client1.send_initial_info()
+
+    # Start the server in a separate thread
+    server_thread = threading.Thread(target=nms_server.listen_for_datagrams, args=(nms_server.UDP_socket,))
+    server_thread.daemon = True
+    server_thread.start()
 
     time.sleep(20)
-
-    client1.close()
-    client1_thread.join()
