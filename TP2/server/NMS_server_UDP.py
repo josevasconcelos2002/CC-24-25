4c44bcb6a6d96b19338eca807c52cf887c9faf14
José Vasconcelos <zevasconcelos6@gmail.com>
2024-12-01 10:45:11 +0000

    NMS UDP

--- TP2/server/NMS_server_UDP.py
+++ TP2/server/NMS_server_UDP.py
@@ -9,7 +9,8 @@ from tasks.task import Task
 from tasks.tasks import Tasks
 from clients.clients import Clients
 from clients.client_server import ClientServer
-from misc.sendMessage import sendMessage, openFile
+from misc.sendMessage import sendMessage
+from misc.openFile import openFile
 import random
 import time
 
@@ -20,6 +21,8 @@ class NMS_server_UDP:
         self.currentT = 0
         self.threads = {}
 
+
+
     def listen_for_datagrams(self, cond, device: Client, socket, addr, task: Task):
         buffer_size = 1024
         if self.currentT == self.maxT:
@@ -29,13 +32,24 @@ class NMS_server_UDP:
         received = False
         socket.settimeout(30)
         sendMessage(socket, addr, task.to_bytes(), 1)
-        file = openFile(task.task_id, device)
+        file =openFile(task.task_id, device)
         while not received:
             try:
                 print(f"Server client listening:\n")
                 data, addre = socket.recvfrom(buffer_size)
+                #time.sleep(0.5)
+                #print("stopping")
+                #received = True
+                #self.currentT -= 1
+                #del self.threads[device]
+                #with cond:
+                    #cond.notify()
 
                 if data:
+                    #self.handle_datagram(data, addr)
+                    
+                    
+                    
                     print(f"Received data length: {len(data)}")
                     payload = data[14:]
                     headers = data[:10]
@@ -47,29 +61,38 @@ class NMS_server_UDP:
                         print(f"\nMETRICS: {payload.decode('utf-8')}\n")
                         file.write(f"METRICS: {payload.decode('utf-8')}\n")
                         file.flush()
-                    elif messageType == 2:
+                    else: 
+
+                     if messageType == 2:
                         print(f"\nRESULTS: {payload.decode('utf-8')}\n")
-                        file.write(f"RESULTS: {payload.decode('utf-8')}\n")
+                        file.write( f"RESULTS: {payload.decode('utf-8')}\n")
                         file.flush()
                         sequence += 1
-                    
+                
+                    """    
+                    else:
+                        print(payload.decode('utf-8'))
+                    """
                     print(f"Sequence: {sequence}\n Sequence_length: {sequence_length}")
                     if sequence == sequence_length: 
                         del self.threads[device]
-                        self.currentT -= 1
+                        self.currentT -=1
                         received = True
-                        print(payload.decode('utf-8') + " hello")
+                        print(payload.decode('utf-8')+" hello")
                         with cond:
                             cond.notify()
+                    
             except Exception as e:
-                print(e)
-                if "timed out" in str(e).lower():
-                    print(f"Timeout occurred in {addr}!")
-                    sequence = 0
-                    sendMessage(socket, addr, task.to_bytes(), 1)
+               print(e)
+               if "timed out" in str(e).lower():
+                print(f"Timeout occured in {addr}!")
+                sequence = 0
+                sendMessage(socket, addr, task.to_bytes(), 1)
             except ConnectionResetError as e:
                 print(f"Connection reset error: {e}")
                 break
             except OSError as e:
                 print(f"OS error (likely socket issue): {e}")
                 break
+            #finally:
+                #break
